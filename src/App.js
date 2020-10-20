6c596c76d0f4f194aaaca91b068c89dc3f02245b
Luiz santos <joluizsantos231293@gmail.com>
2020-10-20 14:00:42 -0300

    Prova React Luiz

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logov76etop.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Prova React Luiz 20/10/2020
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
